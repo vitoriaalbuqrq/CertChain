9d334d21aa79ddd881b7933b0976a38d2243113b
FelipeMedeia <felipemedeia560@gmail.com>
2024-12-31 10:54:16 -0300

    Remover o arquivo .env do repositório e adicionar ao .gitignore

--- backend/scripts/Certification.js
+++ backend/scripts/Certification.js
@@ -1,27 +1,50 @@
 const { ethers } = require("hardhat");
 
 async function main() {
+    const [deployer, organization] = await ethers.getSigners();
+    const orgAddress = organization.address;
+    // console.log("Dono do contrato (Hardhat):", deployer.address);
+    // console.log("Conta da Organização (MetaMask):", orgAddress);
+    // const balance = await ethers.provider.getBalance(deployer.address);
+    // console.log("Raw balance (in Wei):", balance);
+    // const balanceOrg = await ethers.provider.getBalance(organization.address);
+    // console.log("Raw balance for Organization(in Wei):", balanceOrg);
 
-    const Certification = await ethers.getContractFactory("Certification");
+    let Certification = await ethers.getContractFactory("Certification");
     const certification = await Certification.deploy();
+    console.log("Contrato implantado no endereço:", certification.target);
+
+    console.log("Adicionando organização...");
+    const tx = await certification.addOrganization(orgAddress);
+    await tx.wait();
+    console.log("Organização adicionada com sucesso.");
+
+    const isAuthorized = await certification.organizations(orgAddress);
+    console.log("Organização está autorizada?", isAuthorized);
+
+    const certificateId = tx.to;
+    const candidateName = "John Doe";
+    const certificationName = "Blockchain Developer";
+    const orgName = "MetaOrganization";
+    const emissionDate = Date.now();
+    const ipfsHash = "Qm...";
+
+    console.log("Gerando certificado pela organização...");
+
+    const generateTx = await certification.connect(organization).generateCertificate(
+        certificateId, candidateName, certificationName, orgName, emissionDate, ipfsHash
+    );
+    await generateTx.wait();
     
-    await certification.waitForDeployment();
-    console.log(`Contract deployed to: ${certification.target}`);
+    console.log("Certificado gerado com sucesso.");
 
-    const [owner] = await ethers.getSigners();
+    const certificate = await certification.getCertificate(certificateId);
+    console.log("Certificado:", certificate);
 
-    const orgAddress = "0x70997970C51812dc3A010C7d01b50e0d17dc79C8";
-
-    console.log("Dono do contrato:", owner.address);
-    
-    const contractOwner = await certification.owner();
-    if (owner.address === contractOwner) {
-        const tx = await certification.addOrganization(orgAddress);
-        await tx.wait();
-        console.log(`Organização ${orgAddress} adicionada ao contrato.`);
-    } else {
-        console.log("Apenas o dono do contrato pode adicionar organizações.");
-    }
+    const balanceOut = await ethers.provider.getBalance(deployer.address);
+    console.log("Raw balance (in Wei):", balanceOut);
+    const balanceOrgOut = await ethers.provider.getBalance(organization.address);
+    console.log("Raw balance for Organization(in Wei):", balanceOrgOut);
 }
 
 main().catch((error) => {
